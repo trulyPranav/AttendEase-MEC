62134c2f26a5a9807bbfa744966909a6dc013d35
Pranav M <pranavm265@gmail.com>
2024-12-31 23:34:20 +0530

    landing screen glitch

--- lib/UI/Screens/LandingScrren/landing_screen.dart
+++ lib/UI/Screens/LandingScrren/landing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LandingScreen extends StatefulWidget {
   const LandingScreen({super.key});
@@ -9,7 +10,22 @@ class LandingScreen extends StatefulWidget {
 
 class _LandingScreenState extends State<LandingScreen> {
   @override
+  void initState(){
+    super.initState();
+  Future.delayed(const Duration(milliseconds: 3000), () async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('firstTime', false);
+    if (mounted) {
+      Navigator.pop(context);
+    }
+  });
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Placeholder(
+      child: Center(
+        child: Text("LANDING SCREEN"),
+      ),
+    );
   }
 }
