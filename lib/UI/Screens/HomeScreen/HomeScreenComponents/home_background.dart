c592dd630bafc5aa9eddd2d55f68b001a4a0142c
Pranav M <pranavm265@gmail.com>
2025-01-02 18:49:34 +0530

    home screen background done

--- lib/UI/Screens/HomeScreen/HomeScreenComponents/home_background.dart
+++ lib/UI/Screens/HomeScreen/HomeScreenComponents/home_background.dart
@@ -6,7 +6,71 @@ class HomeBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              child: Image.asset(
+                "assets/homescreen/leftBottom.png"
+              ),
+            ),
+            Image.asset(
+              "assets/homescreen/leftTop.png"
+            )
+          ],
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Stack(
+                children: [
+                  Image.asset(
+                    "assets/homescreen/rightLeft.png"
+                  ),
+                  Image.asset(
+                    "assets/homescreen/rightTop.png"
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: Image.asset(
+                      "assets/homescreen/rightBottom.png"
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: 0,
+                left: 45,
+                child: Image.asset(
+                  "assets/homescreen/bottomRight.png"
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Image.asset(
+                  "assets/homescreen/bottomDown.png"
+                ),
+              ),
+              Positioned(
+                bottom: 80,
+                child: Image.asset(
+                  "assets/homescreen/bottomTop.png"
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
